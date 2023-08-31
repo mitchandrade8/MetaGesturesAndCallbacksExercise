6412be63d8060815b1f1fc38fd4cc15cb0f8c2cc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-31 11:24:06 -0600

    Initial Commit

---
+++ MetaGesturesAndCallbacksExercise/MetaGesturesAndCallbacksExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaGesturesAndCallbacksExerciseApp.swift
+//  MetaGesturesAndCallbacksExercise
+//
+//  Created by Mitch Andrade on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaGesturesAndCallbacksExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
